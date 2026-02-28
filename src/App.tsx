73da5852b1a136947a7d8363b1997c103a9917b3
Bryan Nguyen <nguyenhbryan0818@gmail.com>
2026-02-27 21:41:04 -0500

    Inter font

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-    <section className="">
-      <div className="flex flex-row h-screen">
-        <div className="flex flex-col text-slate-200">
-          <h1 className="text-7xl">Hello!</h1>
-          <p className="text-3xl">I'm Bryan Nguyen.</p>
-          <p className="max-w-md">Im a software engineer focused on creating apps that simplify your day.</p>
+      <section className="font-sans">
+        <div className="flex flex-row h-screen">
+          <div className="flex flex-col text-slate-200">
+            <h1 className="text-7xl py-2 text-slate-100">Hello!</h1>
+            <p className="text-3xl pb-2">I'm Bryan Nguyen.</p>
+            <p className="max-w-md text-xl">
+              I'm a Virginia Tech undergraduate majoring in Computer Engineering. I am focused on creating apps that solve
+              problems. I am quick to adapt, constantly learning new things.
+              I've primarily built projects with Next.js and PostgreSQL.{" "}
+            </p>
+          </div>
         </div>
-      </div>
       </section>
     </>
   );
