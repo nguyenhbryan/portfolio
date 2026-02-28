95c6c7c02eca5cdbb35f64f89f12b1e17abf048a
Bryan Nguyen <nguyenhbryan0818@gmail.com>
2026-02-27 21:40:40 -0500

    Made nav into component

---
+++ src/components/nav.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header
+      className="
+  sticky top-0 z-50
+  bg-gradient-to-r from-sky-900/90 to-indigo-900/90
+  backdrop-blur-md
+  border-b border-white/10
+"
+    >
+      <nav className="max-w-6xl mx-auto px-6 py-4 justify-center flex">
+        <div>
+            <NavLink to ="/" className="px-3 hover:text-white">
+            Home 
+            </NavLink>
+          <NavLink to="/projects" className="px-3 hover:text-white">
+            Projects
+          </NavLink>
+          <NavLink to="/about" className="px-3 hover:text-white">
+            About
+          </NavLink>
+          <NavLink to="/contact" className="px-3 hover:text-white">
+            Contact
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,27 +1,10 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/nav";
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen bg-linear-90 from-sky-900 to-indigo-900 text-slate-200">
-      <header className="border-b border-white/10  sticky top-0">
-        <nav className="max-w-6xl mx-auto px-6 py-4 justify-between flex">
-            <NavLink to="/" className="text-white font-semibold">
-              Bryan Nguyen
-            </NavLink>
-          <div>
-            
-            <NavLink to="/projects" className="px-3 hover:text-white">
-              Projects
-            </NavLink>
-            <NavLink to="/about" className="px-3 hover:text-white">
-              About
-            </NavLink>
-            <NavLink to="/contact" className="px-3 hover:text-white">
-              Contact
-            </NavLink>
-          </div>
-        </nav>
-      </header>
+      <Navbar/>
 
       <main className="mx-auto max-w-5xl px-6 py-10">
         {/* This is where each page renders */}
